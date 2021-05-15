9e6f953f6fa39289cd444146288901f3af1bcbe1
Justice <popystone@gmail.com>
2021-05-15 11:53:10 +0100

    Initial Project Setup

---
+++ app/src/main/java/com/pixelart/mavelcomics/ui/comicdetail/ComicDetailFragment.kt
@@ -0,0 +1,73 @@
+package com.pixelart.mavelcomics.ui.comicdetail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.pixelart.mavelcomics.databinding.FragmentComicDetailBinding
+import com.pixelart.mavelcomics.placeholder.PlaceholderContent
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ComicDetailFragment : Fragment() {
+
+    /**
+     * The placeholder content this fragment is presenting.
+     */
+    private var item: PlaceholderContent.PlaceholderItem? = null
+
+    lateinit var itemDetailTextView: TextView
+
+    private var _binding: FragmentComicDetailBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            if (it.containsKey(ARG_ITEM_ID)) {
+                // Load the placeholder content specified by the fragment
+                // arguments. In a real-world scenario, use a Loader
+                // to load content from a content provider.
+                item = PlaceholderContent.ITEM_MAP[it.getString(ARG_ITEM_ID)]
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentComicDetailBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+
+        binding.toolbarLayout?.title = item?.content
+
+        itemDetailTextView = binding.itemDetail
+        // Show the placeholder content as text in a TextView.
+        item?.let {
+            itemDetailTextView.text = it.details
+        }
+
+        return rootView
+    }
+
+    companion object {
+        /**
+         * The fragment argument representing the item ID that this fragment
+         * represents.
+         */
+        const val ARG_ITEM_ID = "item_id"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
